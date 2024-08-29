bfa9ff7c180d31c3c75d72eb0ea18a0daf496cbd
NhiDinh <thaonhi6991@gmail.com>
2024-08-29 21:30:09 +0300

    updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,21 +17,3 @@ const userSchema = new mongoose.Schema({
         required: [true, "Please enter your password"],
     },
 });
-
-// Encrypting password before saving user
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        next();
-    }
-
-    this.password = await bcrypt.hash(this.password, 12);
-});
-
-// Compare user password
-userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-}
-
-const User = mongoose.model("User", userSchema);
-
-export default User;
