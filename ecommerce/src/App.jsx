7825f8292c38b4fab72bbd4ff3ed5f45e2f40e2a
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2024-09-05 10:30:16 +0300

    updated

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -3,15 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Hero from "./Components/pageComponents/Hero.jsx";
 import Navbar from "./Components/pageComponents/Navbar.jsx";
-import Container from "react-bootstrap/esm/Container.js";
-import Row from "react-bootstrap/Row";
-// import './Navbar.module.css';
 
 function App() {
   return (
     <>
-       {/*  <Navbar /> */}
-        <Hero />
+    <h1>Heluuu</h1>
     </>
   );
 }
