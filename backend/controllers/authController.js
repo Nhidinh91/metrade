46307a4c7a0c7b65441b843e2a871201b08c8019
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-09-05 09:25:40 +0300

    test register router and registerController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import User from "../models/userModel.js";
 
-export const register = async (req, res) => {
+// export const register = async (req, res) => {
+export const register = (req, res) => {
+  console.log(req.body);
   res.status(500).json({
     status: "error",
     message: "This route is not yet defined",
--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './configs/database.js';
-import authRoutes from './routes/authRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./configs/database.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -12,13 +12,16 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-
+app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.status(500).json({
+    message: "This is resposne",
+  });
+});
 // Connect to MongoDB
 connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
