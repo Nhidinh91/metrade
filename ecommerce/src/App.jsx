ac556ae403cd6630f8563657e9f2c40625faa57c
Tu Dinh <tu.dinh@metropolia.fi>
2024-09-06 09:02:21 +0300

    chores: decorate and reorganize footer

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     <Navbar/>
     <Hero/>
     <Newsfeed/>
+    <Footer/>
     </>
   );
 }
--- ecommerce/src/Components/pageComponents/Footer.jsx
+++ ecommerce/src/Components/pageComponents/Footer.jsx
@@ -13,14 +13,21 @@ const Footer = () => {
                         <p>Metrade is the exclusive marketplace for the Metropolia University community, designed to help students and staff buy, sell, and trade with ease. Join us in connecting the campus one trade at a time.</p>
                     </Col>
                     <Col md={4}>
-                        <Button className='btn-white'>Contact Us</Button>
+                        <Button className='btn-empty'>Contact Us</Button>
                     </Col>
                 </Row>
             </Col>
 
             <Col className='footer-lower text-center py-3'>
-                
-                <p>&copy; 2021 Metrade. All rights reserved.</p>   
+                <Col>
+                    <a href='https://www.facebook.com' target='_blank' rel='noreferrer'><i className="fa-brands fa-facebook-f"/></a>
+                    <a href='https://www.instagram.com' target='_blank' rel='noreferrer'><i class="fa-brands fa-instagram"></i></a>
+                    <a href='https://www.twitter.com' target='_blank' rel='noreferrer'><i class="fa-brands fa-twitter"></i></a>
+                    <a href='https://www.linkedin.com' target='_blank' rel='noreferrer'><i class="fa-brands fa-linkedin"></i></a>
+                </Col> 
+                <Col>
+                    <p>&copy; Metrade 2024. All rights reserved.</p>   
+                </Col>
             </Col>
         </Container>
     </footer>
