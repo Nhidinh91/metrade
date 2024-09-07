89ec6d23877381517c2f80ee19e13957bbe88c9e
Tu Dinh <tu.dinh@metropolia.fi>
2024-09-07 20:10:40 +0300

    Feat: Navbar with menu and sub menu

--- ecommerce/src/Components/PageLink.jsx
+++ ecommerce/src/Components/PageLink.jsx
@@ -3,11 +3,14 @@ import style from '../Styles/PageLink.module.css';
 
 const PageLink = ({link, itemClass}) => {
   return (
-    <NavDropdown id={link.id} title={link.main_category} className={`${itemClass} ${style.navDropdownToggle}`}>
-      <NavDropdown.Item>{link.sub_categories}</NavDropdown.Item> {/* To be fixed */}
+     <NavDropdown id={link.id} title={link.main_category} className={`${itemClass} ${style.navDropdownToggle}`}>
+      {link.sub_categories.map((subCategory, index) => (
+        <NavDropdown.Item key={index}>{subCategory}</NavDropdown.Item>
+      ))}
     </NavDropdown>
-  )
-}
+  );
+};
+
 
 
 export default PageLink;
--- ecommerce/src/dummyData.js
+++ ecommerce/src/dummyData.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 export const products = [
   {
     _id: 1,
