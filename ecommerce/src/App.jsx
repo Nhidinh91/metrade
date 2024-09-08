c6f67fd30526bad2fb2c88910a5b6593eef4347f
NhiDinh <thaonhi6991@gmail.com>
2024-09-08 15:44:54 +0300

    integrate login component to homepage

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./Pages/Home.jsx";
+import Login from "./Pages/Login.jsx";
 import AboutContact from "./Pages/AboutContact.jsx";
 import SearchResults from "./Pages/SearchResult.jsx";
 
@@ -10,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} /> 
-        <Route path="/login" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Home />} />
         <Route path="/about" element={<AboutContact />} />
         <Route path="/search-results" element={<SearchResults />} />
