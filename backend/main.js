2532751bd6a222aa06ab339adfd454349015a103
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-09-16 04:24:34 +0300

    finish the backend part, able to send required amounts from category, subcategory and sub subcategory

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import connectDB from './configs/database.js';
 import authRoutes from './routes/authRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
@@ -9,20 +10,19 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-
+app.use(morgan('dev'));
 // Middleware
 // A middleware function in Express.js that is used to parse incoming JSON payloads in HTTP requests
 app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/categories', categoryRoutes)
+app.use('/api/categories', categoryRoutes);
 
 // Connect to MongoDB
 connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,13 +1,19 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
 
 //Import category controllers
-import {getProductsByCategory} from '../controllers/productControllers.js'
-import {getCategories} from '../controllers/categoryControllers.js'
+import {
+  getProductsByCategory,
+  getProductsByCategoryV1,
+} from '../controllers/productControllers.js';
+import { getCategories } from '../controllers/categoryControllers.js';
 
 //ROUTES
-router.get('/:categoryId', getProductsByCategory)
-router.get('/', getCategories)
+// router.get("/:categoryId", getProductsByCategory);
 
+//anh thử chạy bằng controller thi nó hiện ra sản phẩm nha
+router.get('/:categoryId', getProductsByCategoryV1);
+// router.get("/trung/:categoryId", getProductsByCategoryV1);
+router.get('/', getCategories);
 
 export default router;
