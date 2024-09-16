817e0c7de60498828272d2f86d6cd62ce7bdfc74
NhiDinh <thaonhi6991@gmail.com>
2024-09-16 16:40:33 +0300

    update

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -10,7 +10,6 @@ import ProductListing from "./Pages/ProductListing.jsx";
 import NotFound from "./Pages/NotFound.jsx";
 import SellingHistory from "./Pages/SellingHistory.jsx";
 import PurchaseHistory from "./Pages/PurchaseHistory.jsx";
-import Profile from "./Pages/Profile.jsx";
 import { AuthProvider } from "./contexts/AuthContext";
 import SignUp from "./Pages/SignUp.jsx";
 import VerifyFail from "./Pages/VerifyFail.jsx";
@@ -35,7 +34,6 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<AboutContact />} />
             <Route path="search-results" element={<SearchResults />} />
-            <Route path="/profile" element={<Profile />} />
             <Route path="/selling-history" element={<SellingHistory />} />
             <Route path="/purchase-history" element={<PurchaseHistory />} />
             <Route path="category" element={<ProductListing />} />
--- ecommerce/src/Pages/MyPage.jsx
+++ ecommerce/src/Pages/MyPage.jsx
@@ -1,30 +1,41 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import "../Styles/MyPage.css";
 import AccountInfo from "../Components/AccountInfo";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const MyPage = () => {
-    return (
-        <Container className="my-page">
-            <h3>My Page</h3>
-            <Row sm={1} md={2} lg={2} className="">
-                <Col sm={12} md={2} lg={2} className="left-container mb-4">
-                    <Link to="/my-page">
-                        <Button className="menu-item-btn">My Page</Button>
-                    </Link>
-                    <Link to="/selling-history">
-                        <Button className="menu-item-btn">My Selling Page</Button>
-                    </Link>
-                    <Link to="/purchase-history">
-                        <Button className="menu-item-btn">Purchase History</Button>
-                    </Link>
-                </Col>
-                <Col sm={12} md={10} lg={10} className="right-container">
-                    <AccountInfo />
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
+  
+  return (
+    <Container className="my-page">
+      <h3>My Page</h3>
+      <Row sm={1} md={2} lg={2} className="">
+        <Col sm={12} md={2} lg={2} className="left-container mb-4">
+          <Link to="/my-page">
+            <Button
+              className="menu-item-btn"
+              disabled={location.pathname === "/my-page"}
+            >
+              My Profile
+            </Button>
+          </Link>
+          <Link to="/selling-history">
+            <Button className="menu-item-btn">My Selling Page</Button>
+          </Link>
+          <Link to="/purchase-history">
+            <Button className="menu-item-btn">My Purchase History</Button>
+          </Link>
+        </Col>
+        <Col sm={12} md={10} lg={10} className="right-container">
+          <AccountInfo />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default MyPage;
