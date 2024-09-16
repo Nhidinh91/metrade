12a8a61c547a18249ae30f881e9f8ee72b26c8f8
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-09-16 22:47:57 +0300

    edit res, return products and categoryObj (parent + child + children)\

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,7 +1,7 @@
-import Product from '../models/productModel.js';
-import mongoose from 'mongoose';
-import { ObjectId } from 'mongodb';
-import Category from '../models/categoryModel.js';
+import Product from "../models/productModel.js";
+import mongoose from "mongoose";
+import { ObjectId } from "mongodb";
+import Category from "../models/categoryModel.js";
 
 export const getProductsByCategory = async (req, res) => {
   const chosenCategory = req.params.categoryId;
@@ -12,13 +12,13 @@ export const getProductsByCategory = async (req, res) => {
     if (products.length > 0) {
       res.status(200).json(products);
     } else {
-      console.log('No products found');
-      res.status(404).json({ message: 'No products found' });
+      console.log("No products found");
+      res.status(404).json({ message: "No products found" });
     }
   } catch (error) {
     console.log(error);
     // Handle any server errors
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -29,8 +29,8 @@ export const getProductsByCategoryV1 = async (req, res) => {
     chosenCategoryId = new mongoose.Types.ObjectId(req.params.categoryId);
   } catch (err) {
     return res.status(400).json({
-      status: 'fail',
-      message: 'invalid category id',
+      status: "fail",
+      message: "invalid category id",
     });
   }
   // console.log("id ", chosenCategoryId);
@@ -42,46 +42,48 @@ export const getProductsByCategoryV1 = async (req, res) => {
       // which are the subcategories of this categories and the sub-subcategories of the subcategories
       {
         $graphLookup: {
-          from: 'categories',
-          startWith: '$_id',
-          connectFromField: '_id',
-          connectToField: 'parent_id',
-          as: 'categoryHierarchy',
+          from: "categories",
+          startWith: "$_id",
+          connectFromField: "_id",
+          connectToField: "parent_id",
+          as: "categoryHierarchy",
         },
       },
     ]);
 
     if (categoryObj.length === 0) {
       return res.status(404).json({
-        status: 'fail',
-        message: 'this category does not exist',
+        status: "fail",
+        message: "this category does not exist",
       });
     }
     //extract the _id of each element inside the catGraph array + the input _id
     //and put into the array called categoriesIdList
     // find the products based on this list $in
     // console.log(categories[0].categoryHierarchy);
-
+    console.log("object array", categoryObj);
+    // const categoryHierarchy = categoryObj[0].categoryHierarchy;
     const categoryIds = categoryObj[0].categoryHierarchy.map(
       (category) => category._id
     );
-    categoryIds.push(chosenCategoryId);
-    // console.log(categoryIds);
+    // categoryIds.push(chosenCategoryId);
+    console.log("category id", categoryIds);
     const products = await Product.find({
       category_id: { $in: categoryIds },
     });
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       count: products.length,
       data: {
         products,
+        categoryObj,
       },
     });
   } catch (err) {
     res.status(500).json({
-      status: 'fail',
-      message: 'Interal server error',
+      status: "fail",
+      message: "Interal server error",
     });
   }
 };
