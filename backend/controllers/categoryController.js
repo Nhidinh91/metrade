f33cd79f88e5869b5bcd9cb4a19beef935b9d2da
Tu Dinh <tu.dinh@metropolia.fi>
2024-09-19 14:37:25 +0300

    Fix feature: Navbar displaying correct category hierachy from backend

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import Category from "../models/categoryModel.js";
 
+//Get all categories in database
 export const getCategories = async (req, res) => {
     try {
         const categories = await Category.find({ is_active: true }).exec();
@@ -10,3 +11,43 @@ export const getCategories = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 }
+
+//Get categories with hierarchy from main -> sub -> sub-sub
+export const categoryHierarchy = async (req, res) => {
+  try {
+    const mainCategories = await Category.find({ parent_id: null }).exec();
+    console.log(mainCategories);
+
+    // Check if main categories exist
+    if (!mainCategories || mainCategories.length === 0) {
+      return res.status(404).json({ message: "No main categories found" });
+    }
+
+    // Populate children for each main category
+    const populatedCategories = await Promise.all(
+      mainCategories.map(async (mainCategory) => {
+        const { _id, name, children } = mainCategory;
+        console.log("subCategories: ", children); //list of subCategories
+
+        let childrenDocs = [];
+        if (children && children.length > 0) {
+          childrenDocs = await Category.find({ _id: { $in: children } })
+            .populate({ path: "children", select: "name _id" })
+            .exec();
+        }
+        // Return the main category with populated children
+        return {
+          _id,
+          name,
+          children: childrenDocs,
+        };
+      })
+    );
+    // Send the populated main categories
+    res.status(200).json(populatedCategories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
--- ecommerce/src/Pages/ProductListing.jsx
+++ ecommerce/src/Pages/ProductListing.jsx
@@ -1,41 +1,48 @@
 import { Container, Row, Col } from 'react-bootstrap';
 // import { products } from '../dummyData';
 import { useSearchParams } from 'react-router-dom';
-import { useEffect, useMemo, useState } from "react";
-import ProductCard from '../Components/ProductCard';  
+import { useEffect, useState } from 'react';
+import Footer from './Footer';
+import ProductCard from '../Components/ProductCard';
+import Header from './Header';
 
-
-const ProductListing = ({ }) => {
+const ProductListing = ({}) => {
   const [searchParams] = useSearchParams();
   const subCategory = searchParams.get('query'); // This is equivalent to the category_id
   const [filteredProducts, setFilteredProducts] = useState([]);
-  console.log("category_id changed to: ", subCategory);
-  
+  console.log('category_id received in ProductListing to: ', subCategory);
+
   // const filteredProducts = products.filter(product => product.category_id === subCategory);
-  
+
   useEffect(() => {
     // Fetch products from backend
     const fetchProducts = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/api/categories/${subCategory}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json(); 
+        const response = await fetch(
+          `http://localhost:3000/api/categories/${subCategory}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        const data = await response.json();
         setFilteredProducts(data);
-        console.log("Filtered products changed to:" ,data);
+        console.log('Filtered products changed to:', data);
       } catch (error) {
-        console.error("Error fetching products:", error);
+        console.error('Error fetching products:', error);
       }
     };
 
     fetchProducts();
   }, [subCategory]);
- 
+
   return (
     <>
+      <Container>
+        <h2>Search Results</h2>
+      </Container>
       <Container>
         {filteredProducts.length > 0 ? (
           <Row sm={2} md={3} lg={4} className="g-4">
