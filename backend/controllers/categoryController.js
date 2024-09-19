1135d7ca682aead5e87829f8daf637c273e730fe
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2024-09-19 16:32:44 +0300

    chore: comment out console.log in categoryController

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -3,20 +3,20 @@ import Category from "../models/categoryModel.js";
 
 //Get all categories in database
 export const getCategories = async (req, res) => {
-    try {
-        const categories = await Category.find({ is_active: true }).exec();
-        res.status(200).json(categories);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-}
+  try {
+    const categories = await Category.find({ is_active: true }).exec();
+    res.status(200).json(categories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 //Get categories with hierarchy from main -> sub -> sub-sub
 export const categoryHierarchy = async (req, res) => {
   try {
     const mainCategories = await Category.find({ parent_id: null }).exec();
-    console.log(mainCategories);
+    //console.log(mainCategories);
 
     // Check if main categories exist
     if (!mainCategories || mainCategories.length === 0) {
@@ -27,7 +27,7 @@ export const categoryHierarchy = async (req, res) => {
     const populatedCategories = await Promise.all(
       mainCategories.map(async (mainCategory) => {
         const { _id, name, children } = mainCategory;
-        console.log("subCategories: ", children); //list of subCategories
+        //console.log("subCategories: ", children); //list of subCategories
 
         let childrenDocs = [];
         if (children && children.length > 0) {
@@ -46,8 +46,7 @@ export const categoryHierarchy = async (req, res) => {
     // Send the populated main categories
     res.status(200).json(populatedCategories);
   } catch (error) {
-    console.log(error);
+    //console.log(error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
-
